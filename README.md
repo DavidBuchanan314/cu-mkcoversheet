d4172479c13c7074750fd6ababcd17ce853ccbdf
DavidBuchanan314 <d@vidbuchanan.co.uk>
2017-03-31 18:24:57 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cu-mkcoversheet
 Generate a coursework coversheet as required by Cardiff University, with a single command.
+
+# Installation:
+Edit the value of the `NUMBER` variable in `mkcoversheet.sh` to match your student number. Then run `sudo ./install.sh`
+
+# Usage:
+```
+USAGE: mkcoversheet MODULE_CODE HOURS_SPENT
+
+```
+
+The submission date field is filled automatically with the current date, in ISO-8601 format.
---
+++ mkcoversheet.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# != 2 ]; then
+	echo "USAGE: $(basename $0) MODULE_CODE HOURS_SPENT"
+	exit 1
+fi
+
+TEMPLATE="template.pdf"
+NUMBER="YOUR_STUDENT_NUMBER_HERE"
+CODE=$1
+DATE=$(date -I) # ISO 8601 FTW
+HOURS=$2
+GROUP=""
+
+if [ ! -f $TEMPLATE ]; then
+	TEMPLATE="/usr/share/mkcoversheet/$TEMPLATE"
+fi
+
+sed "s/\[NUMBER\]/$NUMBER/;s/\[CODE\]/$CODE/;s/\[DATE\]/$DATE/;s/\[HOURS\]/$HOURS/;s/\[GROUP\]/$GROUP/" template.pdf > "$NUMBER.pdf"
+
+echo "$NUMBER.pdf generated successfully"
